e620a834a4fd08b06940949868a503d59246102f
11979 <74593367+11979@users.noreply.github.com>
2021-01-17 18:13:33 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,30 @@
+class box
+{
+    constructor(x,y,w,h){
+        this.width=w;
+        this.height=h;
+        var options ={
+            restitution: 0.8
+           // isStatic: true
+        }
+    this.body=Bodies.rectangle(x,y,this.width,this.height,options);
+    World.add(world,this.body);
+}
+display()
+{
+    var pos= this.body.position;
+    var angle = this.body.angle;
+
+    push();
+
+    translate(pos.x,pos.y);
+
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0,0,this.width,this.height);
+
+    pop();
+
+}
+}
---
+++ ground.js
@@ -0,0 +1,21 @@
+class ground
+{
+    constructor(x,y,width,height){
+        this.width=width;
+        this.height=height;
+        var options ={
+            //restitution: 1
+            isStatic: true
+        }
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    World.add(world,this.body);
+}
+display()
+{
+    var pos= this.body.position;
+    rectMode(CENTER);
+    fill(0);
+    rect(pos.x,pos.y,this.width,this.height);
+
+}
+}
